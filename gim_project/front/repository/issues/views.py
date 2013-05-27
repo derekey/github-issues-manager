03e96ce8bf49c208811bd6b8aab3f6fe50851d42
twidi <s.angel@twidi.com>
2013-05-28 00:01:22 +0200

    Test to display issues

--- gim_project/front/repository/issues/views.py
+++ gim_project/front/repository/issues/views.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from ..views import BaseRepositoryView
 
 
@@ -5,3 +7,23 @@ class IssuesView(BaseRepositoryView):
     name = 'Issues'
     url_name = 'issues'
     template_name = 'front/repository/issues/base.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Set default content for the repository views:
+            - list of available repositories
+            - list of all main views for this repository
+        """
+        context = super(IssuesView, self).get_context_data(**kwargs)
+
+        repository = context['current_repository']
+
+        issues = repository.issues.all()
+        current_issue = choice(issues)
+
+        context.update({
+            'issues': issues,
+            'current_issue': current_issue
+        })
+
+        return context
