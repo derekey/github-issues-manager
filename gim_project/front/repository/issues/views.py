c6a8b4b130b387f1dac6594e5924d170ae103346
twidi <s.angel@twidi.com>
2013-05-29 21:26:36 +0200

    Allow to group by label types

--- gim_project/front/repository/issues/views.py
+++ gim_project/front/repository/issues/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.core.urlresolvers import reverse_lazy
 
-from core.models import Issue, GithubUser
+from core.models import Issue, GithubUser, LabelType
 
 from ..views import BaseRepositoryView
 from ...utils import make_querystring
@@ -45,7 +45,7 @@ class IssuesView(BaseRepositoryView):
                 filters['milestone__isnull'] = True
 
         # the base queryset with the current filter
-        queryset = repository.issues.filter(**filters)
+        queryset = repository.issues.filter(**filters).prefetch_related('labels__label_type')
 
         # now filter by labels
         qs_labels = qs_parts.get('labels', None)
@@ -64,11 +64,28 @@ class IssuesView(BaseRepositoryView):
 
         # do we need to group by a field ?
         qs_group_by = qs_parts.get('group_by', None)
+        group_by_field = None
         if qs_group_by in self.allowed_group_by_fields:
-            group_by_issue_field = {'creator': 'user', 'assigned': 'assignee'}.get(qs_group_by, qs_group_by)
-            order_by.append(group_by_issue_field)
+
+            # group by a simple field
+            group_by_field = {'creator': 'user', 'assigned': 'assignee'}.get(qs_group_by, qs_group_by)
+            order_by.append(group_by_field)
+
+        elif qs_group_by and qs_group_by.startswith('type:'):
+
+            # group by a label type
+            label_type_name = qs_group_by[5:]
+            try:
+                label_type = repository.label_types.get(name=label_type_name)
+            except LabelType.DoesNotExist:
+                pass
+            else:
+                context['group_by_label_type'] = label_type
+                group_by_field = 'label_type_grouper'
+
+        if group_by_field:
             # add the real field in context, to use by the "regroup" templatetag
-            context['group_by_field'] = group_by_issue_field
+            context['group_by_field'] = group_by_field
 
         # and finally, asked ordering
         qs_sort_field = qs_parts.get('sort', None)
@@ -87,6 +104,11 @@ class IssuesView(BaseRepositoryView):
             'sort': qs_sort_field,
             'direction': qs_sort_order,
         })
+        # set the group_by_field if ok, else remove it
+        if group_by_field:
+            context['group_by_field'] = group_by_field
+        else:
+            context['querystring_parts'].pop('group_by', None)
 
         return queryset
 
@@ -146,14 +168,52 @@ class IssuesView(BaseRepositoryView):
         context.update({
             'root_issues_url': issues_url,
             'current_issues_url': issues_url,
-            'issues': issues,
             'issues_filter': issues_filter,
             'collaborators': users_lists['collaborators'],
             'issues_creators': users_lists['issues_creators'],
             'no_assigned_filter_url': repository.get_issues_user_filter_url_for_username('assigned', 'none'),
         })
+        context['issues'] = self.finalize_issues(issues, context)
 
         return context
+
+    def finalize_issues(self, issues, context):
+        """
+        Return a final list of issues usable in the view.
+        Actually simply order ("group") by a label_type if asked
+        """
+        if not issues:
+            return issues
+
+        if context.get('group_by_label_type', None) and context.get('group_by_field', None):
+            attribute = context['group_by_field']
+            label_type = context['group_by_label_type']
+
+            # regroup issues by label from the lab
+            issues_dict = {}
+            for issue in issues.all():
+                add_to = None
+
+                for label in issue.labels.all():  # thanks prefetch_related
+                    if label.label_type_id == label_type.id:
+                        # found a label for the wanted type, mark it and stop
+                        # checking labels for this issue
+                        add_to = label.id
+                        setattr(issue, attribute, label.typed_name)
+                        break
+
+                # add in a dict, with one entry for each label of the type (and one for None)
+                issues_dict.setdefault(add_to, []).append(issue)
+
+            # for each label of the type, append matching issues to the final
+            # list
+            issues = []
+            for label in [None] + list(label_type.labels.all()):
+                label_id = None if label is None else label.id
+                if label_id in issues_dict:
+                    issues += issues_dict[label_id]
+
+        return issues
 
 
 class UserIssuesView(IssuesView):
