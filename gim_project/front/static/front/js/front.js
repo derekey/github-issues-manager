2ac653839476f6b5f70c8eb85b7269ed6a1244f3
twidi <s.angel@twidi.com>
2013-05-30 23:59:55 +0200

    Set current issue in list as active when fetched via ajax

--- gim_project/front/static/front/js/front.js
+++ gim_project/front/static/front/js/front.js
@@ -2,10 +2,13 @@ $().ready(function() {
     var $issue_container = $('#issue');
 
     var on_issue_link_click = (function on_issue_link_click (e) {
+        var link = this;
         e.preventDefault();
         e.stopPropagation();
         $.get(this.href, function(data) {
-            $('#issue').html(data);
+            $issue_container.html(data);
+            $('.issue-item.active').removeClass('active');
+            $(link).closest('.issue-item').addClass('active');
         });
     });
 
