2ad16d03f0e8f7406bd171431c66f6d7e32ff33a
twidi <s.angel@twidi.com>
2013-06-10 18:25:35 +0200

    Display the user's available repositories

--- gim_project/front/dashboard/urls.py
+++ gim_project/front/dashboard/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', login_required(TemplateView.as_view(template_name='front/dashboard/home.html')), name='home'),
+    url(r'^repositories/add/$', login_required(TemplateView.as_view(template_name='front/dashboard/add_repository.html')), name='add_repository'),
 )
