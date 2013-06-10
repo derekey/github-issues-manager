9057245e69922e1e8bc02afb1ba90a6ca36118bc
twidi <s.angel@twidi.com>
2013-06-10 22:14:49 +0200

    Login is required for all dashboard and repositories urls

--- gim_project/front/dashboard/urls.py
+++ gim_project/front/dashboard/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', login_required(TemplateView.as_view(template_name='front/dashboard/home.html')), name='home'),
-    url(r'^repositories/add/$', login_required(TemplateView.as_view(template_name='front/dashboard/add_repository.html')), name='add_repository'),
+    url(r'^$', TemplateView.as_view(template_name='front/dashboard/home.html'), name='home'),
+    url(r'^repositories/add/$', TemplateView.as_view(template_name='front/dashboard/add_repository.html'), name='add_repository'),
 )
--- gim_project/front/urls.py
+++ gim_project/front/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+
+from decorator_include import decorator_include
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='front/home.html'), name='home'),
     url(r'^auth/', include('front.auth.urls', namespace='auth')),
-    url(r'^dashboard/', include('front.dashboard.urls', namespace='dashboard')),
-    url(r'^(?P<owner_username>[^/]+)/(?P<repository_name>[^/]+)/', include('front.repository.urls', namespace='repository'))
+    url(r'^dashboard/', decorator_include(login_required, 'front.dashboard.urls', namespace='dashboard')),
+    url(r'^(?P<owner_username>[^/]+)/(?P<repository_name>[^/]+)/', decorator_include(login_required, 'front.repository.urls', namespace='repository'))
 )
