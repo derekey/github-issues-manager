2e6f2e45d0427dcf5c83698d7cb34b2aeb491f5b
twidi <s.angel@twidi.com>
2013-06-11 17:21:44 +0200

    Remove the list of repositories, they will be displayed on the dashboard

--- gim_project/front/dashboard/repositories/urls.py
+++ gim_project/front/dashboard/repositories/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from .views import AddRepositoryView, ChooseRepositoryView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='front/dashboard/repositories/list.html'), name='list'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy("front:dashboard:home"))),
     url(r'^choose/$', ChooseRepositoryView.as_view(), name='choose'),
     url(r'^add/$', AddRepositoryView.as_view(), name='add'),
 )
