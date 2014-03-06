a926df10b5e45bc3cc01dacfdda8cfd3afb13aa4
twidi <s.angel@twidi.com>
2014-03-06 14:34:37 +0100

    Split issue field forms into mixin
    Each field form include one mixin, but it allows us to have a form with
    many mixins, like the issue creation form

--- gim_project/front/repository/issues/forms.py
+++ gim_project/front/repository/issues/forms.py
@@ -61,31 +61,24 @@ class IssueStateForm(LinkedToUserFormMixin, IssueFormMixin):
         return super(IssueStateForm, self).save(commit)
 
 
-class IssueTitleForm(IssueFormMixin):
-    class Meta(IssueFormMixin.Meta):
-        fields = ['title']
-
+class IssueTitleFormPart(object):
     def __init__(self, *args, **kwargs):
-        super(IssueTitleForm, self).__init__(*args, **kwargs)
+        super(IssueTitleFormPart, self).__init__(*args, **kwargs)
         self.fields['title'].validators = [partial(validate_filled_string, name='title')]
         self.fields['title'].widget = forms.TextInput()
 
 
-class IssueBodyForm(IssueFormMixin):
-    class Meta(IssueFormMixin.Meta):
-        fields = ['body']
+class IssueBodyFormPart(object):
 
     def save(self, commit=True):
         self.instance.body_html = None  # will be reset with data from github
-        return super(IssueBodyForm, self).save(commit)
+        return super(IssueBodyFormPart, self).save(commit)
 
 
-class IssueMilestoneForm(IssueFormMixin):
-    class Meta(IssueFormMixin.Meta):
-        fields = ['milestone']
+class IssueMilestoneFormPart(object):
 
     def __init__(self, *args, **kwargs):
-        super(IssueMilestoneForm, self).__init__(*args, **kwargs)
+        super(IssueMilestoneFormPart, self).__init__(*args, **kwargs)
         milestones = self.repository.milestones.all().order_by('-state', '-number')
         self.fields['milestone'].queryset = milestones
         self.fields['milestone'].widget.choices = self.get_milestones_choices(milestones)
@@ -114,12 +107,9 @@ class IssueMilestoneForm(IssueFormMixin):
         return [('', 'No milestone')] + list(data.items())
 
 
-class IssueAssigneeForm(IssueFormMixin):
-    class Meta(IssueFormMixin.Meta):
-        fields = ['assignee']
-
+class IssueAssigneeFormPart(object):
     def __init__(self, *args, **kwargs):
-        super(IssueAssigneeForm, self).__init__(*args, **kwargs)
+        super(IssueAssigneeFormPart, self).__init__(*args, **kwargs)
         collaborators = self.repository.collaborators.all()
         self.fields['assignee'].queryset = collaborators
         self.fields['assignee'].widget.choices = self.get_collaborators_choices(collaborators)
@@ -144,14 +134,11 @@ class IssueAssigneeForm(IssueFormMixin):
         return [('', 'No one assigned')] + [(u.id, u.username) for u in collaborators]
 
 
-class IssueLabelsForm(IssueFormMixin):
+class IssueLabelsFormPart(object):
     simple_label_name = 'Labels'
 
-    class Meta(IssueFormMixin.Meta):
-        fields = ['labels']
-
     def __init__(self, *args, **kwargs):
-        super(IssueLabelsForm, self).__init__(*args, **kwargs)
+        super(IssueLabelsFormPart, self).__init__(*args, **kwargs)
         labels = self.repository.labels.all().select_related('label_type')
         self.fields['labels'].required = False
         self.fields['labels'].queryset = labels
@@ -185,6 +172,31 @@ class IssueLabelsForm(IssueFormMixin):
         if self.simple_label_name in data:
             data[self.simple_label_name] = data.pop(self.simple_label_name)
         return [(k, sorted(v, key=lambda x: x[1].lower())) for k, v in data.items()]
+
+
+class IssueTitleForm(IssueTitleFormPart, IssueFormMixin):
+    class Meta(IssueFormMixin.Meta):
+        fields = ['title']
+
+
+class IssueBodyForm(IssueBodyFormPart, IssueFormMixin):
+    class Meta(IssueFormMixin.Meta):
+        fields = ['body']
+
+
+class IssueMilestoneForm(IssueMilestoneFormPart, IssueFormMixin):
+    class Meta(IssueFormMixin.Meta):
+        fields = ['milestone']
+
+
+class IssueAssigneeForm(IssueAssigneeFormPart, IssueFormMixin):
+    class Meta(IssueFormMixin.Meta):
+        fields = ['assignee']
+
+
+class IssueLabelsForm(IssueLabelsFormPart, IssueFormMixin):
+    class Meta(IssueFormMixin.Meta):
+        fields = ['labels']
 
 
 class BaseCommentCreateForm(LinkedToUserFormMixin, LinkedToIssueFormMixin):
