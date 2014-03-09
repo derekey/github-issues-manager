c6304e6a0d91bb5bf0f27a6e0ca0a90a68ccd051
twidi <s.angel@twidi.com>
2014-03-09 17:19:28 +0100

    Remove a redirection from repo/ to repo/dashboard then repo/dashboard/

--- gim_project/front/repository/urls.py
+++ gim_project/front/repository/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='dashboard'), name='home'),
+    url(r'^$', RedirectView.as_view(url='dashboard/'), name='home'),
     url(r'^dashboard/', include('front.repository.dashboard.urls')),
     url(r'^issues/', include('front.repository.issues.urls')),
     url(r'^board/', include('front.repository.board.urls')),
