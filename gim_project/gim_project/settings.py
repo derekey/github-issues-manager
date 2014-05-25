31e9fdfb7082d9c10e1e1e016bb1225dc99466e4
twidi <s.angel@twidi.com>
2014-05-25 19:58:52 +0200

    Ultra minimalist admin, for internal use

--- gim_project/gim_project/settings.py
+++ gim_project/gim_project/settings.py
@@ -150,7 +150,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_extensions',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
--- gim_project/gim_project/urls.py
+++ gim_project/gim_project/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^core-admin/', include(admin.site.urls)),
     url(r'^hooks/', include('hooks.urls', namespace='hooks')),
     url(r'^graphs/', include('graphs.urls', namespace='graphs')),
     url(r'^', include('front.urls', namespace='front', app_name='front')),
