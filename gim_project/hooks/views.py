bf383facbda5cdb3cb00e3ec0c2911723dfb6f09
twidi <s.angel@twidi.com>
2014-06-19 19:23:32 +0200

    Manage push events received via hooks
    Fixed #199

--- gim_project/hooks/views.py
+++ gim_project/hooks/views.py
@@ -54,3 +54,6 @@ class GithubWebHook(View):
 
     def event_commit_comment(self, payload):
         return self.event_manager.event_commit_comment(payload['comment'])
+
+    def event_push(self, payload):
+        return self.event_manager.event_push(payload)
